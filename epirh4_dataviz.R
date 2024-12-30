756b0551604200a38fcaa9f42c43981716ead6d1
ronpaul_laptop <ronakpaulpc@yahoo.com>
2024-12-30 22:15:02 +0530

    majcom 241230 new script files

---
+++ epirh4_dataviz.R
@@ -0,0 +1,36 @@
+# Practice code from the "Data vizualization" section of the Epi R Handbook
+# We have created the sections as per the handbook
+# NOTE: Section headers can be entered using ctrl+shift+R or manually typed
+# Each section represents one chapter of the handbook
+
+
+#_====
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# C29 - Tables for Presentation -------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Here we learn how to convert summary data frames into presentation-ready 
+# tables with the flextable package. These tables can be inserted into 
+# powerpoint slides, HTML pages, PDF or Word documents, etc.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ epirh6_misc.R
@@ -0,0 +1,23 @@
+# Practice code from the "Miscellaneous" section of the Epi R Handbook
+# We have created the sections as per the handbook
+# NOTE: Section headers can be entered using ctrl+shift+R or manually typed
+# Each section represents one chapter of the handbook
+
+
+#_====
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# C46 - Version control and collaboration with Git and Github -------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+# TBC ####
+
+
+
+
+
